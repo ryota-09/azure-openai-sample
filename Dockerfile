bbcbc2035656e16f39e6d8cedeff976f44e2cf8c
ryota-09 <n.ryo950911@gmail.com>
2024-01-07 15:16:23 +0900

    response確認

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM python:3.11-buster
 ENV PYTHONUNBUFFERED=1
-WORKDIR /src
+WORKDIR /api
 RUN pip install poetry
 COPY pyproject.toml poetry.lock ./
+COPY . .
 RUN poetry config virtualenvs.in-project true
 RUN if [ -f pyproject.toml ]; then poetry install --no-root; fi
-ENV MY_ENV=${MY_ENV}
+ENV OPENAI_API_KEY=${OPENAI_API_KEY}
 ENTRYPOINT ["poetry", "run", "uvicorn", "api.main:app", "--host", "0.0.0.0", "--reload"]
--- api/main.py
+++ api/main.py
@@ -1,10 +1,33 @@
 from fastapi import FastAPI
 import os
+import openai
 
 app = FastAPI()
 
+openai.api_type = "azure"
+openai.api_base = "https://sample-instance.openai.azure.com/"
+openai.api_version = "2023-07-01-preview"
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+message_text = [
+    {"role":"system","content":"##役割 \n あなたは優秀なコピーライターです。ユーザーが提供するお題に対してのコピーを考えてください。\n ## 制約条件 \n ・コピーは20文字以内です。\n・漢字の割合を30%、ひらがなの割合を70%にします。\n・魅力的かつ印象的なコピーを考えてください。"},
+    { "role": "user", "content": "ゆず味のアイスキャンディー" },
+    { "role": "assistant", "content": "爽やかなゆずの口どけ" },
+    {  "role": "user", "content": "サバ味のソフトクリーム"}
+]
+
 @app.get("/hello")
 async def hello():
-    home = os.environ['MY_ENV']
-    print(home)
-    return {"message": "Hello World"}
+    response = openai.ChatCompletion.create(
+        engine="sampleChatModel1",
+        messages = message_text,
+        temperature=1,
+        max_tokens=800,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None
+    )
+    result = response["choices"][0]["message"]["content"]
+    return {"message": result}
+
